93a14d599b4163b8ffac1c1632fce414c7bbffd0
Alexander Vo <randomcivvy0121@gmail.com>
2020-01-04 13:23:25 -0700

    Add TypeAst types

--- src/structures.rs
+++ src/structures.rs
@@ -7,3 +7,31 @@ pub struct BindingModule {
 
 /// Dependency source path of the **input project, NOT the generated code**
 pub struct Dependency(pub Str);
+
+pub enum TypeAst {
+    Fn(Str, FnType),
+    Binding(BindingType),
+    Array(Box<TypeAst>),
+    Primitive(PrimitiveType),
+}
+
+pub enum PrimitiveType {
+    Boolean,
+    Number,
+    String,
+    Void,
+    Object,
+    Any,
+    Never,
+}
+
+/// Primitive types are not type-aliasable
+pub struct BindingType {
+    name: Str,
+    origin: Str,
+}
+
+pub struct FnType {
+    params: Vec<TypeAst>,
+    return_type: Option<Box<TypeAst>>,
+}
