32bfc822577e3ad54f0d8313d3e481f29ac50b41
Alexander Vo <randomcivvy0121@gmail.com>
2020-01-19 17:02:40 -0700

    Minimal compiling bind_graph module

--- src/bind_graph.rs
+++ src/bind_graph.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
 use swc_ecma_ast::*;
+use swc_common::Span;
 
 use super::bind_init;
 use super::error::*;
@@ -50,6 +51,7 @@ pub fn build_module_graph(
         let mut module_info = ModuleInfo::new(canon_path.into());
 
         hoist_imports(&mut module.module_ast);
+        todo!("Build module graph");
 
     }
 
@@ -81,17 +83,19 @@ fn hoist_imports(module: &mut Module) {
 struct BindGenSession;
 
 impl BindGenSession {
+
     fn process_module_item(
         &mut self,
         context: &mut Context,
         module_info: &mut ModuleInfo,
         item: ModuleItem,
-    ) -> Result<(), BindGenError> {
+        ) -> Result<(), BindGenError> {
+
 
         match item {
             ModuleItem::ModuleDecl(decl) => self.process_module_decl(context, module_info, decl),
 
-            ModuleItem::Stmt(stmt) => todo!(),
+            ModuleItem::Stmt(stmt) => todo!("ModuleItem::Stmt"),
         }
     }
 
@@ -110,8 +114,6 @@ impl BindGenSession {
                 span,
                 specifiers,
             }) => {
-                let dep_module_info = self.bind_module_from_src(context, &src, Some(span))?;
-
                 for specifier in specifiers {
                     match specifier {
                         ImportSpecifier::Specific(ImportSpecific {
@@ -119,21 +121,13 @@ impl BindGenSession {
                             local,
                             imported,
                         }) => {
-                            context.value_scope.try_import(
-                                &dep_module_info,
-                                local.sym.to_string(),
-                                imported.as_ref().map(|ident| ident.sym.to_string())
-                            );
-
-                            context.type_scope.try_import(
-                                &dep_module_info,
-                                local.sym.to_string(),
-                                imported.map(|ident| ident.sym.to_string())
-                            );
+                            // context.value_scope.insert(
+                            todo!("Insert nebulous value + type");
                         }
 
                         ImportSpecifier::Default(def) => {
                             return Err(BindGenError {
+                                module_path: module_info.path().to_owned(),
                                 kind: BindGenErrorKind::UnsupportedFeature(UnsupportedFeature::DefaultImport),
                                 span: def.span,
                             });
@@ -141,6 +135,7 @@ impl BindGenSession {
 
                         ImportSpecifier::Namespace(namespace) => {
                             return Err(BindGenError {
+                                module_path: module_info.path().to_owned(),
                                 kind: BindGenErrorKind::UnsupportedFeature(UnsupportedFeature::DefaultImport),
                                 span: namespace.span,
                             });
@@ -162,18 +157,23 @@ impl BindGenSession {
                 match item_kind {
                     ItemKind::Value => {
                         module_info.export_value(name.clone(), typ.clone());
-                        context.value_scope.insert(name, Value::Rooted(typ));
+                        // context.value_scope.insert(name, Value::Rooted(typ));
+                        todo!("Insert rooted value");
                     }
 
                     ItemKind::Type => {
                         module_info.export_type(name.clone(), typ.clone());
-                        context.type_scope.insert(name, typ);
+                        // context.type_scope.insert(name, typ);
+                        todo!("Insert rooted type");
                     }
+
                     ItemKind::ValueType => {
                         module_info.export_value(name.clone(), typ.clone());
                         module_info.export_type(name.clone(), typ.clone());
-                        context.value_scope.insert(name.clone(), Value::Rooted(typ.clone()));
-                        context.type_scope.insert(name, typ);
+                        // context.value_scope.insert(name.clone(), Value::Rooted(typ.clone()));
+                        // context.type_scope.insert(name, typ);
+
+                        todo!("Insert rooted type+value");
                     }
                 }
 
@@ -191,13 +191,8 @@ impl BindGenSession {
                     // Open the source module and re-export an exported item
                     Some(src) => {
 
-                        let dep_module_info = self.bind_module_from_src(context, &src, Some(span))?;
-
                         Box::new(move |original_key: String, as_key: Option<String>| -> () {
-                            module_info.merge_export(
-                                        &dep_module_info,
-                                        original_key,
-                                        as_key);
+                            todo!("Export as a nebulous value + type");
                         })
                     }
 
@@ -219,11 +214,13 @@ impl BindGenSession {
                             }
 
                             if let Some(value_type) = value_type {
-                                module_info.export_value(as_key.clone(), value_type);
+                                // module_info.export_value(as_key.clone(), value_type);
+                                todo!("Export as a rooted value");
                             }
 
                             if let Some(type_item) = type_item {
-                                module_info.export_type(as_key, type_item);
+                                // module_info.export_type(as_key, type_item);
+                                todo!("Export as a rooted value");
                             }
                         })
                     }
@@ -249,6 +246,7 @@ impl BindGenSession {
                             ..
                         }) => {
                             return Err(BindGenError {
+                                module_path: module_info.path().to_owned(),
                                 kind: BindGenErrorKind::UnsupportedFeature(
                                           UnsupportedFeature::NamespaceExport),
                                 span,
@@ -257,6 +255,7 @@ impl BindGenSession {
 
                         ExportSpecifier::Default(..) => {
                             return Err(BindGenError {
+                                module_path: module_info.path().to_owned(),
                                 kind: BindGenErrorKind::UnsupportedFeature(
                                           UnsupportedFeature::DefaultExport),
                                 span,
@@ -273,51 +272,337 @@ impl BindGenSession {
                 span,
                 ..
             }) => {
-                let dep_module_info = self.bind_module_from_src(context, &src, Some(span))?;
 
                 // Take all exports and merge into the current module
-                module_info.merge_all(&*dep_module_info);
+                // module_info.merge_all(&*dep_module_info);
+                todo!("Add to module info's export all");
 
                 Ok(())
             }
 
-            ModuleDecl::ExportDefaultDecl(ExportDefaultDecl { ref span, .. }) => {
-                Err(BindGenError {
-                    kind: BindGenErrorKind::UnsupportedFeature(UnsupportedFeature::DefaultExport),
-                    span: span.clone(),
-                })
-            }
+            ModuleDecl::ExportDefaultDecl(..)
+                => unreachable!("Caught in init"),
 
-            ModuleDecl::ExportDefaultExpr(ExportDefaultExpr { ref span, .. }) => {
-                Err(BindGenError {
-                    kind: BindGenErrorKind::UnsupportedFeature(UnsupportedFeature::DefaultExport),
-                    span: span.clone(),
-                })
-            }
+            ModuleDecl::ExportDefaultExpr(..)
+                => unreachable!("Caught in init"),
 
-            ModuleDecl::TsImportEquals(TsImportEqualsDecl { ref span, .. }) => {
-                Err(BindGenError {
-                    kind: BindGenErrorKind::UnsupportedFeature(UnsupportedFeature::TsImportEquals),
-                    span: span.clone(),
-                })
-            }
+            ModuleDecl::TsImportEquals(..)
+                => unreachable!("Caught in init"),
 
-            ModuleDecl::TsExportAssignment(TsExportAssignment { ref span, .. }) => {
-                Err(BindGenError {
-                    kind: BindGenErrorKind::UnsupportedFeature(UnsupportedFeature::TsExportAssignment),
-                    span: span.clone(),
-                })
-            }
+            ModuleDecl::TsExportAssignment(..)
+                => unreachable!("Caught in init"),
 
-            ModuleDecl::TsNamespaceExport(TsNamespaceExportDecl { ref span, .. }) => {
+            ModuleDecl::TsNamespaceExport(..)
+                => unreachable!("Caught in init"),
+        }
+    }
 
-                // TODO: Handle TsNamespaceExport?
-                //   What is TsNamespaceExport??
-                Err(BindGenError {
-                    kind: BindGenErrorKind::UnsupportedFeature(UnsupportedFeature::TsNamespaceExport),
-                    span: span.clone(),
+    fn process_decl(
+        context: &Context,
+        module_info: &ModuleInfo,
+        decl: Decl,
+        span: Span,
+        ) -> Result<Item, BindGenError> {
+
+        match decl {
+            Decl::Class(ClassDecl {
+                ident,
+                class: Class {
+                    span,
+                    body,
+                    type_params,
+                    ..
+                },
+                ..
+            }) => {
+                // TODO: Type parameters
+                // TODO: Subtyping relations?
+                // TODO: Implements?
+
+                todo!();
+            },
+
+            Decl::Fn(FnDecl {
+                ident,
+                function: Function {
+                    span,
+                    params,
+                    is_generator,
+                    is_async,
+                    return_type,
+                    type_params,
+                    ..
+                },
+                ..
+            }) => {
+                // TODO: Type parameters
+
+                let params = params
+                    .into_iter()
+                    .map(|p| {
+                        BindGenSession::type_from_pattern(context, module_info, p)
+                    })
+                .collect::<Result<Vec<_>, _>>()?;
+
+                let return_type = return_type
+                    .map(|ann| BindGenSession::type_from_ann(context, module_info, ann))
+                    .transpose()?
+                    .map(|typ| Box::new(typ));
+
+                let fn_type = Type::Fn {
+                    origin: module_info.path().display().to_string(),
+                    type_signature: FnType {
+                        params,
+                        return_type,
+                    }
+                };
+
+                Ok(Item::Fn {
+                    name: ident.sym.to_string(),
+                    typ: fn_type,
                 })
-            }
+            },
+
+            Decl::Var(VarDecl {
+                decls,
+                ..
+            }) => {
+                todo!();
+            },
+
+            Decl::TsInterface(TsInterfaceDecl {
+                id,
+                span,
+                body,
+                type_params,
+                ..
+            }) => {
+                // TODO: Type parameters
+                // TODO: Implements?
+
+                todo!();
+            },
+
+            Decl::TsTypeAlias(TsTypeAliasDecl {
+                span,
+                id,
+                type_ann,
+                type_params,
+                ..
+            }) => {
+                // TODO: Type parameters
+
+                let aliasing_type = BindGenSession::bind_type(context, module_info, *type_ann)?;
+                let name = id.sym.to_string();
+
+                Ok(Item::TsTypeAlias{
+                    name: name.clone(),
+                    typ: Type::Alias {
+                        name,
+                        aliasing_type: Box::new(aliasing_type),
+                    }
+                })
+            },
+
+            Decl::TsEnum(TsEnumDecl {
+                span,
+                id,
+                ..
+            }) => {
+                // TODO: Care about inhabitants?
+
+                todo!();
+            },
+
+            Decl::TsModule(..) => {
+                todo!("TS modules not sup");
+            },
+        }
+    }
+
+    fn type_from_pattern(
+        context: &Context,
+        module_info: &ModuleInfo,
+        pattern: Pat,
+    ) -> Result<Type, BindGenError> {
+        // TODO: Perform basic type inference?
+        let ann: Option<_> = match pattern {
+            Pat::Ident(ident) => ident.type_ann,
+            Pat::Array(array_pat) => array_pat.type_ann,
+            Pat::Rest(rest_pat) => rest_pat.type_ann,
+            Pat::Object(object_pat) => object_pat.type_ann,
+            Pat::Assign(assign_pat) => assign_pat.type_ann,
+            Pat::Invalid(invalid) => todo!("Invalid pattern {:?}", invalid),
+            Pat::Expr(expr) => todo!("What is an expr pattern"),
+        };
+
+        ann
+            .map(|ann| BindGenSession::type_from_ann(context, module_info, ann))
+            .unwrap_or(Ok(Type::Primitive(PrimitiveType::Any)))
+    }
+
+    fn type_from_ann(
+        context: &Context,
+        module_info: &ModuleInfo,
+        ann: TsTypeAnn,
+    ) -> Result<Type, BindGenError> {
+        let ann_span = ann.span;
+
+        BindGenSession::bind_type(context, module_info, *ann.type_ann)
+    }
+
+    fn bind_type(
+        context: &Context,
+        module_info: &ModuleInfo,
+        typ: TsType,
+    ) -> Result<Type, BindGenError> {
+
+        match typ {
+            TsType::TsKeywordType(TsKeywordType {
+                span,
+                kind,
+            }) => {
+
+                let prim_type = match kind {
+                    TsKeywordTypeKind::TsAnyKeyword => PrimitiveType::Any,
+                    TsKeywordTypeKind::TsUnknownKeyword => todo!("unknown keyword type"),
+                    TsKeywordTypeKind::TsNumberKeyword => PrimitiveType::Number,
+                    TsKeywordTypeKind::TsObjectKeyword => PrimitiveType::Object,
+                    TsKeywordTypeKind::TsBooleanKeyword => PrimitiveType::Boolean,
+                    TsKeywordTypeKind::TsBigIntKeyword => todo!("big int keyword type"),
+                    TsKeywordTypeKind::TsStringKeyword => PrimitiveType::String,
+                    TsKeywordTypeKind::TsSymbolKeyword => todo!("symbol keyword type"),
+                    TsKeywordTypeKind::TsVoidKeyword => PrimitiveType::Void,
+                    TsKeywordTypeKind::TsUndefinedKeyword => PrimitiveType::Undefined,
+                    TsKeywordTypeKind::TsNullKeyword => PrimitiveType::Null,
+                    TsKeywordTypeKind::TsNeverKeyword => PrimitiveType::Never,
+                };
+
+                Ok(Type::Primitive(prim_type))
+            },
+
+            TsType::TsThisType(TsThisType {
+                span,
+            }) => {
+                todo!("What is TsThisType?");
+            },
+
+            TsType::TsFnOrConstructorType(TsFnOrConstructorType::TsFnType(TsFnType {
+                span,
+                params,
+                type_params,
+                type_ann,
+            })) => {
+                // What is type_ann
+                // Is type_ann the return type?
+                todo!("ts fn");
+            },
+
+            TsType::TsFnOrConstructorType(TsFnOrConstructorType::TsConstructorType(TsConstructorType {
+                span,
+                params,
+                type_params,
+                type_ann,
+            })) => {
+                // What is type_ann
+                // Is type_ann the return type?
+                todo!("ts constructor");
+            },
+
+            TsType::TsTypeRef(TsTypeRef {
+                span,
+                type_name,
+                type_params,
+            }) => {
+                todo!();
+            },
+
+            TsType::TsTypeQuery(_TsTypeQuery) => {
+                todo!("ts type query");
+            },
+
+            TsType::TsTypeLit(..) => {
+                todo!("ts type literals not supported");
+            },
+
+            TsType::TsArrayType(TsArrayType {
+                span,
+                elem_type,
+            }) => {
+                let elem_type = Box::new(BindGenSession::bind_type(context, module_info, *elem_type)?);
+                Ok(Type::UnsizedArray(elem_type))
+            },
+
+            TsType::TsTupleType(TsTupleType {
+                span,
+                elem_types,
+            }) => {
+                // Tuple types are fixed-length arrays (at init)
+                todo!("ts tuple type");
+            },
+
+            TsType::TsOptionalType(..) => {
+                todo!("ts optional types not supported");
+            },
+
+            TsType::TsRestType(..) => {
+                todo!("ts rest types not supported");
+            },
+
+            TsType::TsUnionOrIntersectionType(TsUnionOrIntersectionType::TsUnionType(TsUnionType {
+                span,
+                types,
+            })) => {
+                // TODO: How to bind union types?
+                // Keep opaque for now
+                Ok(Type::Union)
+            },
+
+            TsType::TsUnionOrIntersectionType(TsUnionOrIntersectionType::TsIntersectionType(..)) => {
+                todo!("ts intersection types not supported");
+            },
+
+            TsType::TsConditionalType(..) => {
+                todo!("ts conditional types not supported");
+            },
+
+            TsType::TsInferType(..) => {
+                todo!("ts infer type not supported");
+            },
+
+            TsType::TsParenthesizedType(TsParenthesizedType {
+                span,
+                type_ann,
+            }) => {
+                todo!("parenthesized type");
+            },
+
+            TsType::TsTypeOperator(_TsTypeOperator) => {
+                todo!("type operators not supported");
+            },
+
+            TsType::TsIndexedAccessType(_TsIndexedAccessType) => {
+                todo!("ts indexed access type not supported");
+            },
+
+            TsType::TsMappedType(_TsMappedType) => {
+                todo!("ts mapped type not supported");
+            },
+
+            TsType::TsLitType(TsLitType {
+                span,
+                lit,
+            }) => {
+                todo!("ts lit type");
+            },
+
+            TsType::TsTypePredicate(_TsTypePredicate) => {
+                todo!("ts type predicates not supported?");
+            },
+
+            TsType::TsImportType(_TsImportType) => {
+                todo!("What is TsImportType?");
+            },
         }
     }
 }
