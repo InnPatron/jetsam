228c00c798babbc8a8f205c99378717f37513c08
Alexander Vo <randomcivvy0121@gmail.com>
2020-02-14 16:05:56 -0800

    Output JS overrides. Emit class constructor wrappers behind the output_constructor_wrappers flag

--- src/js_emit.rs
+++ src/js_emit.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 
 use super::type_structs::*;
 use super::emit::EmitOptions;
+use super::emit_common;
 
 pub struct JsOutput<'a> {
     options: &'a EmitOptions,
@@ -21,7 +22,51 @@ impl<'a> JsOutput<'a> {
     }
 
     pub fn handle_type(&mut self, name: &str, typ: &Type) {
-        // Do nothing for now
+        match typ {
+            Type::Class(ref class_type) => {
+                opt!(self.options, output_constructor_wrappers, {
+
+                    for (index, constructor) in class_type.constructors.iter().enumerate() {
+
+                        let constructor_name =
+                            emit_common::constuctor_name(index, &*class_type.name);
+
+                        let string_constructor =
+                            self.build_constructor(&*class_type.name, &constructor_name, constructor);
+
+                        self.overrides.insert(constructor_name, string_constructor);
+                    }
+                });
+            }
+
+            _ => (),
+        }
+    }
+
+    fn build_constructor(&self,
+        class_name: &str,
+        constructor_name: &str,
+        constructor: &FnType
+    ) -> String {
+
+        let list = {
+            let mut params = String::new();
+            for (index, _) in constructor.params.iter().enumerate() {
+                params.push('p');
+                params.push_str(&index.to_string());
+
+                if index < constructor.params.len() - 1 {
+                    params.push(',');
+                }
+            }
+
+            params
+        };
+
+        let params = &list;
+        let body = format!("return new {}({});", class_name, list);
+
+        format!("function {}({}) {{ {} }}", constructor_name, params, body)
     }
 
     pub fn finalize(self, default_require_path: String) -> String {
@@ -37,8 +82,14 @@ impl<'a> JsOutput<'a> {
         );
 
         output.push_str(
-            &format!("module.exports = root.exports;")
+            &format!("module.exports = root.exports;\n\n")
         );
+
+        for (override_key, override_value) in self.overrides.into_iter() {
+            output.push_str(
+                &format!("module.exports[\"{}\"] = {};\n", override_key, override_value)
+            );
+        }
 
         output
     }
