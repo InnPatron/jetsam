03d61bdd69a5d5f38146be13bda44655c87f5f4d
Alexander Vo <randomcivvy0121@gmail.com>
2020-07-23 22:32:13 -0700

    Begin filling out TsFlavor and TsFlavorBuilder. Update TsFlavor::ts_num()

--- src/ts.rs
+++ src/ts.rs
@@ -1,15 +1,72 @@
 #[derive(Debug, PartialEq, Eq, Clone, Builder)]
 pub struct TsFlavor {
     #[builder(default = "false")]
-    pub number: bool,
+    pub number_type: bool,
+
+    #[builder(default = "false")]
+    pub boolean_type: bool,
+
+    #[builder(default = "false")]
+    pub string_type: bool,
+
+    #[builder(default = "false")]
+    pub array_type: bool,
+
+    #[builder(default = "false")]
+    pub tuple_type: bool,
+
+    #[builder(default = "false")]
+    pub explicit_enum_type: bool,
+
+    #[builder(default = "false")]
+    pub object_type: bool,
+
+    #[builder(default = "false")]
+    pub any_type: bool,
+
+    #[builder(default = "false")]
+    pub fn_type: bool,
+
+    #[builder(default = "false")]
+    pub fn_declarations: bool,
+
+    #[builder(default = "false")]
+    pub interfaces: bool,
+
+    #[builder(default = "false")]
+    pub interface_extension: bool,
+
+    #[builder(default = "false")]
+    pub recursive_interface: bool,
+
+    #[builder(default = "false")]
+    /// Ex: funciton foo (x: { a: number}) { .. }
+    /// => type annotation for x is a type literal
+    pub type_literal: bool,
+
+    #[builder(default = "false")]
+    pub literal_type: bool,
 }
 
 impl TsFlavor {
 
     pub fn ts_num() -> Self {
         TsFlavorBuilder::default()
-            .number(true)
+            .number_type(true)
+            .fn_type(true)
+            .allow_simple_records()
+            .fn_declarations(true)
             .build()
             .expect("ts_num failed")
+    }
+}
+
+impl TsFlavorBuilder {
+    pub fn allow_simple_records(&mut self) -> &mut Self {
+        self
+            .interfaces(true)
+            .type_literal(true)
+            .interface_extension(false)
+            .recursive_interface(false)
     }
 }
