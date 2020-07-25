f2744d0a9fe1ac13d04b5f5f90742223d0568846
Alexander Vo <randomcivvy0121@gmail.com>
2020-07-25 01:19:16 -0700

    Update test ts_num::i_test()

--- tests/ts_num.rs
+++ tests/ts_num.rs
@@ -2,6 +2,14 @@
 mod macros;
 mod common;
 
+const BINDING_DIR: &'static str = "src/bindings";
+
+macro_rules! binding_file {
+    ($f: expr) => {
+        format!("{}/{}", BINDING_DIR, $f)
+    }
+}
+
 #[test]
 fn i_test() {
     common::check_aux_bins().unwrap();
@@ -9,11 +17,13 @@ fn i_test() {
     let test_env = common::TestEnv::new("i_test");
 
     test_env.create_tmp_dir("src");
-    test_env.create_tmp_dir("build");
-    test_env.create_tmp_file("src/basic_ts_num.d.ts", include_str!("./data/basic_ts_num.d.ts"));
+    test_env.create_tmp_dir("src/bindings");
+    test_env.create_tmp_dir(BINDING_DIR);
+    test_env.create_tmp_file(binding_file!("basic_ts_num.d.ts"), include_str!("./data/basic_ts_num.d.ts"));
+    test_env.create_tmp_file(binding_file!("basic_ts_num.js"), include_str!("./data/basic_ts_num.js"));
 
 
-    let mut build_cmd = test_env.jetsam_build_cmd("src/basic_ts_num.d.ts", "build");
+    let mut build_cmd = test_env.jetsam_build_cmd(binding_file!("basic_ts_num.d.ts"), BINDING_DIR);
     let output = build_cmd
         .arg("--ts-flavor")
         .arg("ts-num")
@@ -21,6 +31,7 @@ fn i_test() {
         .expect("jetsam failed (i/o error)");
 
     if !output.status.success() {
+        dbg!(test_env);
         return panic!("Command `{:?}` failed with code: {}", build_cmd, output.status);
     }
 }
