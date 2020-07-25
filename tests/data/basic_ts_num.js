4feb3923a8dc1e493c44a36e70e5bd34e1380b61
Alexander Vo <randomcivvy0121@gmail.com>
2020-07-25 00:30:58 -0700

    Add basic_ts_num test files

---
+++ tests/data/basic_ts_num.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "foo": function foo(x) {
+        return x * 2;
+    },
+
+    "bar": function bar(x, y) {
+        return x + y;
+    },
+
+    "qux": function zoo(x) {
+        return x(-100, -100);
+    },
+
+    "zoo": function qux(x) {
+        return function(a1) {
+            return x(a1, 100);
+        }
+    }
+};
---
+++ tests/data/basic_ts_num_runner.arr
@@ -0,0 +1,2 @@
+import global as G
+import js-file("./binds/basic_ts_num") as NUM
