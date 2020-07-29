bb6c4a384ef3c5e38850ce1bbebdee9863abcc03
Alexander Vo <randomcivvy0121@gmail.com>
2020-07-29 13:27:48 -0700

    Add Default implementation for GenConfig.
    Add doc comments

--- src/generate/config.rs
+++ src/generate/config.rs
@@ -1,6 +1,24 @@
 pub struct GenConfig {
+
+    /// Define functions that wrap class constructors (expiremental)
+    /// Used by:
+    ///     * TS-FULL
     pub output_constructor_wrappers: bool,
+
+    /// Define 1:1 opaque nominal datatypes in Pyret per exported TS interface
+    /// Used by:
+    ///     * TS-FULL
     pub output_opaque_interfaces: bool,
+
+}
+
+impl Default for GenConfig {
+    fn default() -> Self {
+        GenConfig {
+            output_constructor_wrappers: true,
+            output_opaque_interfaces: true,
+        }
+    }
 }
 
 pub struct EmitConfig {
