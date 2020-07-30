96408b324cdea9bf1e427e6b51669d5614e319cc
Alexander Vo <randomcivvy0121@gmail.com>
2020-07-30 13:13:19 -0700

    Add test config_unwrapped_ts_num
    Loads a configuration file and turns off exported variable wrapping

--- tests/ts_num.rs
+++ tests/ts_num.rs
@@ -20,3 +20,21 @@ make_test!(BASIC(unwrapped_ts_num)
     pyret-compile: |_, c| c;
     => expects: common::line_separated_expected(&["40", "-20", "-55", "99", "9000", "Done"])
 );
+
+make_test!(FULL(test => config_unwrapped_ts_num, data => unwrapped_ts_num)
+    jetsam-compile: |env: &common::TestEnv, mut c: std::process::Command| {
+        env.create_tmp_file(
+            "config.json",
+            include_str!("./data/unwrapped_ts_num.config1.json")
+        );
+
+
+        c
+            .arg("--base-config")
+            .arg(env.get_tmp_path("config.json"));
+
+        c
+    };
+    pyret-compile: |_, c| c;
+    => expects: common::line_separated_expected(&["40", "-20", "-55", "99", "9000", "Done"])
+);
