b95a800863c6bf7cf44fc730f36ed8a1312fb9ac
Alexander Vo <randomcivvy0121@gmail.com>
2020-07-30 10:54:39 -0700

    Update comments

--- src/common.rs
+++ src/common.rs
@@ -22,11 +22,11 @@ pub const DEFAULT_TS_FLAVOR: (TsFlavor, &'static str) = (TsFlavor::TsNum, "TS-NU
 
 const_str!(OPTION_TS_FLAVOR => "ts-flavor");
 
-/// Needs to be kept in sync with `GenConfig.output_constructor_wrappers` serde name
+// Needs to be kept in sync with `GenConfig.output_constructor_wrappers` serde name
 const_str!(OPTION_CONSTRUCTOR_WRAPPERS => "constructor-wrappers");
 
-/// Needs to be kept in sync with `GenConfig.output_opaque_interfaces` serde name
+// Needs to be kept in sync with `GenConfig.output_opaque_interfaces` serde name
 const_str!(OPTION_OPAQUE_INTERFACES => "opaque-interfaces");
 
-/// Needs to be keep in sync with `GenConfig.wrap_top_level_vars` serde name
+// Needs to be keep in sync with `GenConfig.wrap_top_level_vars` serde name
 const_str!(OPTION_WRAP_TOP_LEVEL_VARS => "wrap-top-level-vars");
